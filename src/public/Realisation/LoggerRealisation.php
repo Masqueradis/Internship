4bd794518acef68fae1138d62cc1889f9d853576
Masqueradis <masqueradisu@gmail.com>
2025-11-05 12:58:25 +0300

    Improved logger

--- src/public/Realisation/LoggerRealisation.php
+++ src/public/Realisation/LoggerRealisation.php
@@ -11,13 +11,19 @@ use Realisation\Logger\Logger;
 $logFile = sys_get_temp_dir() . '/myapp_log.txt';
 $logger = new Logger($logFile);
 
-$logger->error("Failed to connect {username} to {database}", ['database' => 'mysql', 'username' => 'localhost']);
+$logger->error("Failed to connect {username} to {database}", 
+['database' => 'mysql', 
+'username' => 'localhost']);
 echo "<br>";
-$logger->warning("Query is taking too long - {duration} seconds", ['duration' => '3.2']);
+$logger->warning("Query is taking too long - {duration} seconds", 
+['duration' => '3.2']);
 echo "<br>";
-$logger->info("User {username} has logged in" , ['username' => 'localhost']);
+$logger->info("User {username} has logged in" , 
+['username' => 'localhost']);
 echo "<br>";
-$logger->debug("Executing SQL: {query} with params: {par}", ['query' => 'SELECT * FROM users WHERE status = ?', 'par' => 'active']);
+$logger->debug("Executing SQL: {query} with params: {par}", 
+['query' => 'SELECT * FROM users WHERE status = ?', 
+'par' => 'active']);
 
 
 echo "<br>" . "<a href='../index.php'>Back</a>";
