df40cf5c2aacb4360670ea5f9bf76c662b4b0090
Masqueradis <masqueradisu@gmail.com>
2025-11-05 13:02:52 +0300

    Added index.php

---
+++ src/public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PSR</title>
+</head>
+<body>
+    <a href="Realisation/AnimalRealisation.php">Animals</a><br>
+    <a href="Realisation/LoggerRealisation.php">Logger</a><br>
+    <a href="Realisation/EmailRealisation.php">Email</a><br>
+</body>
+</html>
