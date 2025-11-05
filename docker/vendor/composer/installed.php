4804b505ddb1b0bf422c64984a658e3f7973b5aa
Masqueradis <masqueradisu@gmail.com>
2025-11-05 19:45:29 +0300

    Updated composer

--- docker/vendor/composer/installed.php
+++ docker/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'f3be6b675413474c7ee1fbd778f8cf59905feec2',
+        'reference' => '63b49d7e602063177dcc57c1436e4f4714ff0c03',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'f3be6b675413474c7ee1fbd778f8cf59905feec2',
+            'reference' => '63b49d7e602063177dcc57c1436e4f4714ff0c03',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
